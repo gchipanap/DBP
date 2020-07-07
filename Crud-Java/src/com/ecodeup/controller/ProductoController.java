a996463325178d1186c65c677c2ce7322c2b08bc
Gabriela <64268942+gchipanap@users.noreply.github.com>
2020-07-07 03:50:41 -0500

    Crud con Java, Maven, Mysql

---
+++ Crud-Java/src/com/ecodeup/controller/ProductoController.java
@@ -0,0 +1,102 @@
+package com.ecodeup.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecodeup.dao.ProductoDAO;
+import com.ecodeup.model.Producto;
+
+/**
+ * Servlet implementation class ProductoController
+ */
+@WebServlet(description = "Administra peticion", urlPatterns = { "/productos" })
+public class ProductoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductoController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String opcion = request.getParameter("opcion");
+		
+		if(opcion.equals("crear")) {
+			System.out.println("opcion crear presionada");
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("/views/crear.jsp");
+			requestDispatcher.forward(request, response);
+		}else if(opcion.equals("listar")) {
+			
+			ProductoDAO productoDAO=new ProductoDAO();
+			List<Producto> lista = new ArrayList<>();
+			try {
+				lista=productoDAO.obtenerProductos();
+				for(Producto producto : lista) {
+					System.out.println(producto);
+				}
+				request.setAttribute("lista",lista);
+				
+				
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/listar.jsp");
+				requestDispatcher.forward(request, response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("opcion listar precionada");
+			
+		}
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		Date fechaActual = new Date();
+		
+		ProductoDAO productoDAO = new ProductoDAO(); 
+		Producto producto = new Producto();
+		producto.setNombre(request.getParameter("nombre"));
+		producto.setCantidad(Double.parseDouble(request.getParameter("cantidad")));
+		producto.setPrecio(Double.parseDouble(request.getParameter("precio")));
+		producto.setFechaCrear(new java.sql.Date(fechaActual.getTime()));
+		
+		try {
+			productoDAO.guardar(producto);
+			System.out.println("Registro guardado");
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+			requestDispatcher.forward(request, response);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//doGet(request, response);
+	}
+
+}
---
+++ Crud-Java/src/com/ecodeup/dao/ProductoDAO.java
@@ -0,0 +1,98 @@
+package com.ecodeup.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ecodeup.conexion.Conexion;
+import com.ecodeup.model.Producto;
+
+public class ProductoDAO {
+		private Connection connection;
+		private PreparedStatement statement;
+		private boolean estadoOperacion;
+		
+		//guardar
+		public boolean guardar(Producto producto) throws SQLException {
+			String sql=null;
+			estadoOperacion=false;
+			connection=obtenerConexion();
+			
+			try {
+				connection.setAutoCommit(false);
+				sql="INSERT INTO productos VALUES(NULL,?,?,?,?,?)";
+				statement=connection.prepareStatement(sql);
+				
+				statement.setString(1, producto.getNombre());
+				statement.setDouble(2, producto.getCantidad());
+				statement.setDouble(3, producto.getPrecio());
+				statement.setDate(4, producto.getFechaCrear());
+				statement.setDate(5, producto.getFechaActualizar());
+				
+				estadoOperacion=statement.executeUpdate()>0;
+				
+				connection.commit();
+				statement.close();
+				connection.close();
+			} catch (SQLException e) { 
+				connection.rollback();
+				e.printStackTrace();
+			}
+			
+			return true;
+		}
+		
+		//editar
+		public boolean editar(Producto producto) {
+			return true;
+		}
+		
+		//eliminar
+		public boolean eliminar(Producto producto) {
+			return true;
+		}
+		
+		//obtener lista
+		public List<Producto> obtenerProductos() throws SQLException{
+			ResultSet resultSet = null;
+			List<Producto> listaProductos= new ArrayList<>();
+
+			String sql = null;
+			estadoOperacion = false;
+			connection=obtenerConexion();
+
+			try {
+				sql = "SELECT * FROM productos";
+				statement=connection.prepareStatement(sql);
+				resultSet = statement.executeQuery(sql);
+				while (resultSet.next()) {
+					Producto p=new Producto();
+					p.setId(resultSet.getInt(1));
+					p.setNombre(resultSet.getString(2));
+					p.setCantidad(resultSet.getDouble(3));
+					p.setPrecio(resultSet.getDouble(4));
+					p.setFechaCrear(resultSet.getDate(5));
+					p.setFechaActualizar(resultSet.getDate(6));
+					listaProductos.add(p);
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			return listaProductos;
+		}
+		
+		//
+		public Producto obtenerProducto(int idProducto) {
+			return null;
+		}
+		
+		//obtener conexion
+		private Connection obtenerConexion() throws SQLException {
+			return Conexion.getConnection();
+		}
+}
