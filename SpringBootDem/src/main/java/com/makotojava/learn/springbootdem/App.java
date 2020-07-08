7c2adb478fd65584db483c982b4ca7e7916bc757
Gabriela <64268942+gchipanap@users.noreply.github.com>
2020-07-08 00:01:19 -0500

    Add files via upload

---
+++ SpringBootDem/src/main/java/com/makotojava/learn/springbootdem/App.java
@@ -0,0 +1,13 @@
+package com.makotojava.learn.springbootdem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
